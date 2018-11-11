d940999360b6198b3f77ac6efb63bc6cc8fbed8d
Noriko Yoshizawa <roaming_moewe@gmail.com>
2018-11-11 23:03:21 +0900

    create category form to db

--- admin/categories.php
+++ admin/categories.php
@@ -17,6 +17,29 @@
                         </h1>
 
                         <div class="col-xs-6">
+
+<?php
+if (isset($_POST['submit'])) {
+    $cat_title = $_POST['cat_title'];
+    if (empty($cat_title)) {
+        echo "This field should not be empty";
+    } else {
+        $query = "INSERT INTO categories(cat_title) ";
+        $query .= "VALUE(' {$cat_title} ') ";
+
+        $create_category_query = mysqli_query($connection, $query);
+
+        if (!$create_category_query) {
+            die('QUERY FAILED' . mysqli_error());
+        }
+    }
+
+
+}
+
+?>
+
+
                         <form action="" method="post">
                             <div class="form-group">
                                 <label for="cat-title">Add Category</label>
@@ -29,6 +52,12 @@
                         <!-- Add Category Form -->
 
                            <div class="col-xs-6">
+<?php
+$query = "SELECT * FROM categories";
+$select_categories = mysqli_query($connection, $query);
+?>
+
+
                             <table class="table table-bordered table-hover">
                                 <thead>
                                     <tr>
@@ -37,12 +66,18 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td>Baseball Category</td>
-                                        <td>Basketball Category</td>
-                                    </tr>                                   
-
-
+                                    
+  <?php
+    while ($row = mysqli_fetch_assoc($select_categories)) {
+        $cat_id = $row['cat_id'];
+        $cat_title = $row['cat_title'];
+        echo "<tr>";
+        echo "<td>{$cat_id}</td>";
+        echo "<td>{$cat_title}</td>";
+        echo "</tr>";
+    }
+    ?>
+                                  
                                 </tbody>
                             </table>
                            </div>
