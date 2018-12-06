9a16d576803c1925132bb58435908c5df9b6c611
Noriko Yoshizawa <yoshizawa@icue.jp>
2018-12-06 23:59:49 +0900

    ファイル作成、form作成

---
+++ admin/includes/add_post.php
@@ -0,0 +1,38 @@
+<form action="" method="post" enctype="multipart/form-data">
+
+<div class="form-group">
+    <label for="title">投稿タイトル</label>
+    <input type="text" class="form-control" name="title">
+</div>
+
+<div class="form-group">
+    <label for="post_category">投稿カテゴリーID</label>
+    <input type="text" class="form-control" name="post_category_id">
+</div>
+
+<div class="form-group">
+    <label for="author">投稿者</label>
+    <input type="text" class="form-control" name="author">
+</div>
+
+<div class="form-group">
+    <label for="post_status">投稿ステータス</label>
+    <input type="text" class="form-control" name="post_status">
+</div>
+
+<div class="form-group">
+    <label for="post_image">投稿画像</label>
+    <input type="file" name="image">
+</div>
+
+<div class="form-group">
+    <label for="post_tag">投稿タグ</label>
+    <input type="text" class="form-control" name="post_tag">
+</div>
+
+<div class="form-group">
+    <label for="post_content">投稿コンテンツ</label>
+    <textarea class="form-control" name="post_content" id="" cols="30" rows="10"></textarea>
+</div>
+
+</form>
