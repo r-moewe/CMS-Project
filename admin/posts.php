ea8a51fca48035af3a7cc33bc3947cb92b11a3b5
Noriko Yoshizawa <yoshizawa@icue.jp>
2019-02-27 18:50:38 +0900

    諸々修正

--- admin/posts.php
+++ admin/posts.php
@@ -1,9 +1,9 @@
 <?php include 'includes/admin_header.php'; ?>
 
-    <div id="wrapper">
+<div id="wrapper">
 
     <!-- Navigation -->
-<?php include 'includes/admin_navigation.php'; ?>
+    <?php include 'includes/admin_navigation.php'; ?>
     <div id="page-wrapper">
 
         <div class="container-fluid">
@@ -16,62 +16,43 @@
                         <small>Author Name</small>
                     </h1>
 
-<table class="table table-bordered table-hover">
-    <thead>
-            <th>ID</th>
-            <th>Author</th>
-            <th>タイトル</th>
-            <th>カテゴリー</th>
-            <th>状態</th>
-            <th>Image</th>
-            <th>タグ</th>
-            <th>コメント</th>
-            <th>日付</th>
-        
-    </thead>
-    <tbody>
-    <?php
+                    <?php
 
-    $query = "SELECT * FROM posts";
-    $select_posts = mysqli_query($connection, $query);
+                    if (isset($_GET['source'])) {
+                        $source = $_GET['source'];
+                    } else {
+                        $source = '';
+                    }
+                    switch ($source) {
+                        case 'add_post':
+                            include "includes/add_post.php";
+                            break;
 
-    while ($row = mysqli_fetch_assoc($select_posts)) {
-        $post_id = $row['post_id'];
-        $post_author = $row['post_author'];
-        $post_title = $row['post_title'];
-        $post_category_id = $row['post_category_id'];
-        $post_status = $row['post_status'];
-        $post_image = $row['post_image'];
-        $post_tags = $row['post_tags'];
-        $post_count = $row['post_comment_count'];
-        $post_date = $row['post_date'];
+                        case '35':
+                            echo "nice35";
+                            break;
 
-        echo "<tr>";
-        echo "<td>$post_id</td>";
-        echo "<td>$post_author</td>";
-        echo "<td>$post_title</td>";
-        echo "<td>$post_category_id</td>";
-        echo "<td>$post_status</td>";
-        echo "<td>$post_image</td>";
-        echo "<td>$post_tags</td>";
-        echo "<td>$post_count</td>";
-        echo "<td>$post_date</td>";
-        echo "</tr>";
-    }
-    ?>
+                        case '36':
+                            echo "nice36";
+                            break;
+                        default:
 
-    </tbody>
-</table>
+                            include "view_all_posts.php";
+                            break;
+                    }
 
 
-                    </div>
+
+                    ?>
+
                 </div>
-                <!-- /.row -->
-
             </div>
-            <!-- /.container-fluid -->
+            <!-- /.row -->
 
         </div>
-        <!-- /#page-wrapper -->
+        <!-- /.container-fluid -->
 
-<?php include 'includes/admin_footer.php'; ?>
+    </div>
+    <!-- /#page-wrapper -->
+
+    <?php include 'includes/admin_footer.php'; ?> 
